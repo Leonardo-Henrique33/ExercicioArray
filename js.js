57d989b95cf02ea38c1ddf5978cc8e52d4ab5062
Leonardo Subires <sozisobrennolotier99@gmail.com>
2026-05-27 19:23:38 -0300

    :bulb: foi comentado: funcionalidades do while, \n, e break

--- js.js
+++ js.js
@@ -1,9 +1,10 @@
-let numero = []
 
 let numerosDigitados = [];
 for (let i = 0; i < 5; i++) {
 
     while (true) {
+    //Este while se repetirá enquanto essa condição for verdadeira, logo caso não seja (else) é necessário "brecar" ela.
+    while (true) { 
         numero = Number(prompt("Digite um número"))
 
         if (isNaN(numero)
@@ -12,16 +13,15 @@ for (let i = 0; i < 5; i++) {
 
         } else {
             break;
+            break;// Break vai parar o lopping while
         }
     }
     numerosDigitados[i] = { numeroInserido: numero }
-
-
-} 
-
+@@ -23,6 +23,6 @@ for (let i = 0; i < 5; i++) {
     let mensagem = "Os números inseridos foram; \n"
 for (let i = 0; i < numerosDigitados.length; i++) {
 
     mensagem += "\n" + numerosDigitados[i].numeroInserido + "\n"
+    mensagem += "\n" + numerosDigitados[i].numeroInserido + "\n" // "\n" Serve para quebrar uma linha na mensagem exibida.
 
 } alert(mensagem)
