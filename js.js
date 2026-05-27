cad52aa0aabd41b647c840830e04af353afc8436
Leonardo Subires <sozisobrennolotier99@gmail.com>
2026-05-27 19:53:48 -0300

    :sparkles: feat: Finalizado o EX5 (Desafio)

--- js.js
+++ js.js
@@ -2,72 +2,71 @@ let produtos = [];
 let somaProdutos = 0;
 let preco;
 let nome;
+
 let precoMenor = {
     valor: 0,
     produto: 0,
 }
+
 let precoMaior = {
     valor: 0,
     produto: 0,
 }
-for (let i = 0; i < 5; i++) {
 
+for (let i = 0; i < 5; i++) {
 
     while (true) {
         nome = prompt("Digite o nome do produto: ")
 
-        //Trim remove os espaços da variável!
         if (nome.trim() == "") {
-
             alert("Digite um produto válido")
         } else {
-            break; // Vai parar o lopping 
+            break;
         }
-
     }
 
+    while (true) {
+        preco = Number(prompt("Digite o preço do produto"))
 
-    if (preco < precoMenor.valor || preco === 0) {
+        if (isNaN(preco)) {
+            alert("Você não digitou um número")
+        } else {
+            break;
+        }
+    }
+
+    if (preco < precoMenor.valor || precoMenor.valor === 0) {
         precoMenor.valor = preco
         precoMenor.produto = i
     }
-    if (preco > precoMaior.valor || preco === 0) {
+
+    if (preco > precoMaior.valor || precoMaior.valor === 0) {
         precoMaior.valor = preco
         precoMaior.produto = i
     }
 
-    while (true) {
-        preco = Number(prompt("digite o preço do produto"))
-        if (isNaN(preco)) {
-
-            alert("Você não digitou um número")
-
-        } else { break; }
-
-    }
-
-
-
     produtos[i] = {
         nomeProduto: nome,
         precoProduto: preco,
-
     }
-
-
 }
 
-// \n quebra de linha do alert
-let mensagem = "=== PRODUTOS ===\n\n"
+let mensagem = "=== PRODUTOS MAIS CAROS QUE R$15 ===\n\n"
 
 for (let i = 0; i < produtos.length; i++) {
+
     somaProdutos = somaProdutos + produtos[i].precoProduto
-    mensagem += "Produto: " + produtos[i].nomeProduto + "\n";
-    mensagem += "Preço: R$" + produtos[i].precoProduto + "\n";
-    mensagem += "===================== \n"
-    
+
+    if (produtos[i].precoProduto > 15) {
+
+        mensagem += "Produto: " + produtos[i].nomeProduto + "\n";
+        mensagem += "Preço: R$" + produtos[i].precoProduto.toFixed(2) + "\n";
+        mensagem += "=====================\n";
+    }
 }
 
-mensagem += "A Soma dos produtos é: " + somaProdutos
-alert(mensagem); 
-alert(`O preço maior foi R$${precoMaior.valor.toFixed(2)}, e o preço menor foi R$${precoMenor.valor.toFixed(2)} `)
+mensagem += "\nA soma dos produtos é: R$" + somaProdutos.toFixed(2)
+
+alert(mensagem);
+
+alert(`O preço maior foi R$${precoMaior.valor.toFixed(2)}, e o preço menor foi R$${precoMenor.valor.toFixed(2)}`)
