9138cef808e5abcd0830bd85619ee01666a6cdc5
Leonardo Subires <sozisobrennolotier99@gmail.com>
2026-05-27 19:18:37 -0300

    :sparkles: feat: Realizado o primeiro exercício na branch EX1

---
+++ js.js
@@ -0,0 +1,28 @@
+let numero = []
+
+let numerosDigitados = [];
+for (let i = 0; i < 5; i++) {
+
+    while (true) {
+        numero = Number(prompt("Digite um número"))
+
+        if (isNaN(numero)
+        ) {
+            alert("Você não digitou um número")
+
+        } else {
+            break;
+        }
+    }
+    numerosDigitados[i] = { numeroInserido: numero }
+
+
+} 
+
+
+    let mensagem = "Os números inseridos foram; \n"
+for (let i = 0; i < numerosDigitados.length; i++) {
+
+    mensagem += "\n" + numerosDigitados[i].numeroInserido + "\n"
+
+} alert(mensagem)
